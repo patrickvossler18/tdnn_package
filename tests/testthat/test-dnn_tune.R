a0bc30ee64703dd35b2af9e2c248539f471a18b5
Patrick Vossler <patrick.vossler18@gmail.com>
2022-06-15 10:05:24 -0700

    remove libraries from test file

--- tests/testthat/test-dnn_tune.R
+++ tests/testthat/test-dnn_tune.R
@@ -1,12 +1,9 @@
 # source_file("old_implementation.R")
 
 set.seed(1234)
-library(tidyverse)
-library(parallel)
-library(kknn)
 
 dgp_function = function(x){
-    # This function will take a row of the matrix as input and return the 
+    # This function will take a row of the matrix as input and return the
     # transformed value. To use this on a matrix, we can use the apply function
     # with margin=1
     (x[1]-1)^2 + (x[2]+1)^3 - 3*x[3]
@@ -38,21 +35,21 @@ boot_reps <- lapply(1:B, function(b) {
     # Make train data and validation data
     X_train <- X[-B.index[b],]
     Y_train <- as.matrix(Y[-B.index[b],])
-    
+
     X_val <- matrix(X[B.index[b],], 1, ncol(X))
     Y_val <- as.matrix(Y[B.index[b],])
-    
+
     n_train = nrow(X_train)
     p_train = ncol(X_train)
-    
+
     X_train_dis = X_train - kronecker(matrix(1, n_train, 1), X_val)
     EuDis_train = (X_train_dis ^ 2) %*% matrix(1, p_train, 1)
     index.order = sort(EuDis_train, index = T)$ix
     ordered_Y_train <- Y_train[index.order]
-    
-    dnn.res = pmap_df(tidyr::expand_grid(c = 1, s =  s_seq), function(c, s){
+
+    dnn.res = purrr::pmap_df(tidyr::expand_grid(c = 1, s =  s_seq), function(c, s){
         est.dnn = dnn_ord(ordered_Y_train, n_train, p_train, s)
-        bind_rows(list(data.frame(
+        dplyr::bind_rows(list(data.frame(
             MSE.dnn = (est.dnn - Y_val)^2*exp(- sum((X_val - X_test_fixed)^2) / scale_p),
             s = s,
             y_val = Y_val
